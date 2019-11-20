3c33d6ddb323f97b3a932f01d00c613bba9091bc
david <djd1283@gmail.com>
2019-11-20 15:46:02 -0500

    First commit. main.py evaluates random vectors.

---
+++ main.py
@@ -0,0 +1,25 @@
+import senteval
+import numpy as np
+import os
+
+PATH_TO_SENTEVAL = '/home/ddonahue/SentEval/data'
+
+params_senteval = {'task_path': PATH_TO_SENTEVAL, 'usepytorch': True, 'kfold': 10, 'batch_size': 64,
+                   'classifier': {'nhid': 0, 'optim': 'adam', 'batch_size': 64, 'tenacity': 5, 'epoch_size': 4}}
+
+
+def prepare(params, samples):
+    return
+
+def batcher(params, batch):
+    return np.random.randn(len(batch), 100)
+
+se = senteval.engine.SE(params_senteval, batcher, prepare)
+transfer_tasks = ['STS12']
+results = se.eval(transfer_tasks)
+
+for key in results:
+    print(key, results[key]['all'])
+    print()
+
+print('Hello world')
